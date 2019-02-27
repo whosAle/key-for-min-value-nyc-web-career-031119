2171f295372827157efc510bcb0ffa073f9c3a1b
A Belgrave <abelgrave95@gmail.com>
2019-02-27 04:04:44 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,12 +2,8 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  lowest = 0
-  name_hash.each do |k, v|
-    if v < lowest
-      lowest = v
-    end
-  end #{ |k, v| puts lowest }#v < lowest ? lowest = v : lowest}
-  puts lowest
+  lowest = name_hash[0]
+  name_hash.each { |k, v| lowest = v if v < lowest }
+  # puts lowest
   name_hash.invert[lowest]
 end
