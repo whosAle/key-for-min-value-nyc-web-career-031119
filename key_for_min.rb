da50cc5b56844c7e5c4ed5c692fcd4b563a4f18b
A Belgrave <abelgrave95@gmail.com>
2019-02-27 04:02:16 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,7 +3,11 @@
 
 def key_for_min_value(name_hash)
   lowest = 0
-  name_hash.each { |k, v| puts lowest }#v < lowest ? lowest = v : lowest}
+  name_hash.each do |k, v|
+    if v < lowest
+      lowest = v
+    end
+  end #{ |k, v| puts lowest }#v < lowest ? lowest = v : lowest}
   # puts lowest
   name_hash.invert[lowest]
 end
